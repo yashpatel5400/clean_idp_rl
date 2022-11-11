f6ea38815d6bb57caf124b14c06815e4f512555d
yashpatel5400 <ypatel5400@gmail.com>
2022-11-10 21:46:18 -0500

    Commence RL debugging -- this may suck real hard

--- examples/run_param_tune.py
+++ examples/run_param_tune.py
@@ -9,6 +9,7 @@ curriculum_agent_reward_threshs = [
 curriculum_agent_success_rates = [
     .5, .55, .6, .65
 ]
+gpus_to_use = [0, 1, 3, 4, 5]
 
 scheduling_thresh = 4000
 
@@ -18,13 +19,16 @@ for curriculum_agent_reward_thresh in curriculum_agent_reward_threshs:
             gpus = gpu_check.getGPUs()
             found = False
             for gpu_idx, gpu in enumerate(gpus):
+                if gpu_idx not in gpus_to_use:
+                    continue
+
                 if gpu.memoryFree > scheduling_thresh:
                     bashCommand = f"CUDA_VISIBLE_DEVICES={gpu_idx} PYTHONPATH=/home/yppatel/misc/clean_idp_rl/src/ python examples/chignolin_example.py --curriculum_agent_reward_thresh {curriculum_agent_reward_thresh} --curriculum_agent_success_rate {curriculum_agent_success_rate}"
                     print(bashCommand)
                     
-                    subprocess.run(['bash','-c', bashCommand])
-                    # subprocess.Popen([bashCommand], shell=True, stdin=None, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-                    time.sleep(20) # wait for GPU scheduling to occur (compute normalizers)
+                    # subprocess.run(['bash','-c', bashCommand])
+                    subprocess.Popen([bashCommand], shell=True, stdin=None, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                    time.sleep(30) # wait for GPU scheduling to occur (compute normalizers)
                     found = True
                     break
             
